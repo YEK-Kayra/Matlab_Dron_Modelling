c73f66cf0cd9bbfe5d075947a92a77716ac19d2c
YEK-Kayra <124110070+YEK-Kayra@users.noreply.github.com>
2025-06-06 00:43:28 +0300

    Update

--- SIP_QU_Project_V1/SIP_SystemConstant.m
+++
@@ -1,7 +0,0 @@
-
-% System Constants for Drone-Pendulum Model
-
-M_drone = 1.50;   % kg, Mass of the drone (without pendulum)
-mc      = 0.65;   % kg, Mass of the pendulum (without drone)
-l       = 0.30;   % m,  Length of the pendulum stick
-g       = 9.81;   % m/s^2, Gravitational acceleration
